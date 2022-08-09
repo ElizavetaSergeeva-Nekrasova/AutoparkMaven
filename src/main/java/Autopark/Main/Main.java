f9d60f347ab6ac1eec2e59b3979c882dcbed4f30
Elizaveta Sergeeva-Nekrasova <elizaveta.sergeevanekrasova@gmail.com>
2022-08-09 10:07:20 +0300

    test new classes

--- src/main/java/Autopark/Main/Main.java
+++ src/main/java/Autopark/Main/Main.java
@@ -1,6 +1,8 @@
 package Autopark.Main;
 
 import Autopark.Infrastructure.core.impl.ApplicationContext;
+import Autopark.Service.BadMechanicService;
+import Autopark.Service.Fixer;
 import Autopark.Service.Workroom;
 import Autopark.Vehicle.Vehicle;
 import Autopark.VehicleCollections.VehicleCollection;
@@ -12,12 +14,13 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) {
         Map<Class<?>, Class<?>> interfaceToImplementation = new HashMap<>();
+        interfaceToImplementation.put(Fixer.class, BadMechanicService.class);
         ApplicationContext applicationContext = new ApplicationContext("Autopark", interfaceToImplementation);
 
         VehicleCollection vehicleCollection = applicationContext.getObject(VehicleCollection.class);
         Workroom workroom = applicationContext.getObject(Workroom.class);
 
         List<Vehicle> vehicleList = vehicleCollection.getVehicleList();
-
+        workroom.checkAllVehicle(vehicleList);
     }
 }
